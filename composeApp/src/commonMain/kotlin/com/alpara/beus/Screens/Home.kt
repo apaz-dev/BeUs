0a05f75a6afc9bdef5b1435642e9fa843872f653
apaz <apaz-pri@student.42madrid.com>
2026-01-19 12:51:55 +0100

    * new commit: GoodPading

--- composeApp/src/commonMain/kotlin/com/alpara/beus/Screens/Home.kt
+++ composeApp/src/commonMain/kotlin/com/alpara/beus/Screens/Home.kt
@@ -3,10 +3,13 @@ package com.alpara.beus.Screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,12 +22,20 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun HomeScreen() {
     Column(
         modifier = Modifier
-            .safeDrawingPadding()
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
     ) {
-        Text("Pantalla de Inicio", style = MaterialTheme.typography.headlineMedium)
-        Spacer(modifier = Modifier.height(16.dp))
-        Text("Aquí va el contenido de tu pantalla principal")
+        Column(
+            modifier = Modifier
+                .padding(
+                    top = WindowInsets.systemBars.asPaddingValues().calculateTopPadding(),
+                    start = 7.dp,
+                    end = 7.dp
+                )
+        ) {
+            Text("Pantalla de Inicio", style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("Aquí va el contenido de tu pantalla principal")
+        }
     }
 }
