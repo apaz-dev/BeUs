e63cebe9a9657bb08d36c09b66c8fc7774c0235f
raul <raulaxx14@gmail.com>
2026-01-29 19:05:11 +0100

    *new commit: configurationScreen with out backend

--- composeApp/src/commonMain/kotlin/com/alpara/beus/Screens/Main/Profile.kt
+++ composeApp/src/commonMain/kotlin/com/alpara/beus/Screens/Main/Profile.kt
@@ -1,37 +1,7 @@
 package com.alpara.beus.Screens.Main
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@Preview
-@Composable
-fun ProfileScreen(
-    onLogout: () -> Unit = {}
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Perfil de Usuario",
-            style = MaterialTheme.typography.headlineMedium
-        )
 
-        Spacer(modifier = Modifier.height(32.dp))
 
-        Button(
-            onClick = onLogout,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Cerrar Sesión")
-        }
-    }
-}
+
+
