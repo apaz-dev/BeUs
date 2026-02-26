a77e2756a6e43d0b5ef8d8d14bbbcdb5d26b54d0
am12DAM2 <ordenador@local.es>
2026-02-26 11:54:25 +0100

    * new commit: buildgradle update

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -117,8 +117,8 @@ android {
         applicationId = "com.alpara.beus"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "0.1"
+        versionCode = 4
+        versionName = "0.2"
 
         buildConfigField("String", "SUPABASE_URL", "\"${localProperties["SUPABASE_URL"] ?: ""}\"")
         buildConfigField("String", "SUPABASE_ANON_KEY", "\"${localProperties["SUPABASE_ANON_KEY"] ?: ""}\"")
